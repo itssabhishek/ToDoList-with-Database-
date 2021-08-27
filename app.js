f142d12fde945fff6878b30b901f0541984dbde9
Abhishek0710 <you@example.com>
2021-08-27 21:10:00 +0530

    Footer added

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://admin-Abhishek:9839177870@cluster0.vluhz.mongodb.net/todolistDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const itemsSchema = {
   name: String,
